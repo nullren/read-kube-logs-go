af3198c6f571d2a4490b6a1869bc25229259adad
Renning Bruns <rbruns@slack-corp.com>
2023-03-30 09:53:44 -0700

    simple reader

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var kubeconfig = flag.String("c", "/etc/kube/config", "location of kubeconfig to read")
+var namespace = flag.String("n", "default", "namespace of the pod")
+var pod = flag.String("p", "", "pod name")
 
 func main() {
-	fmt.Println("hello")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	req := clientset.CoreV1().Pods(*namespace).GetLogs(*pod, &v1.PodLogOptions{})
+	podLogs, err := req.Stream(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer podLogs.Close()
+
+	scanner := bufio.NewScanner(podLogs)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
